ba642a06dfa8a6abd6cdaedcd4cff775218db85a
Death-thekidd <ohiemidivine7@gmail.com>
2023-02-14 01:23:39 +0100

    [update]: learnt how to use frequency counter

---
+++ frequency_counter_pattern/same_naive.js
@@ -0,0 +1,16 @@
+"use-strict";
+const same = (arr1, arr2) => {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		let correctIndex = arr2.indexOf(arr1[i] ** 2);
+		if (correctIndex === -1) {
+			return false;
+		}
+		arr2.splice(correctIndex, 1);
+	}
+	return true;
+};
+
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
